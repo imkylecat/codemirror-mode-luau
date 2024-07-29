83d91ce854a5b852d4e06c0b4b5c5b01a9dbddc1
Kyle <kyle@kylerich.dev>
2024-07-29 14:51:14 -0400

    Start

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# codemirror-mode-luau
+[![npm](https://img.shields.io/npm/dm/codemirror-mode-luau.svg)](https://www.npmjs.com/package/codemirror-mode-luau)
---
+++ index.js
@@ -0,0 +1,54 @@
+import CodeMirror from "codemirror";
+
+CodeMirror.defineMode("luau", function () {
+    function longComment(stream, state) {
+        while (!stream.eol()) {
+            if (stream.match(/\]\]/)) {
+                state.tokenize = null;
+                break;
+            }
+            stream.next();
+        }
+        return "comment";
+    }
+
+    return {
+        startState: function () {
+            return { tokenize: () => {} };
+        },
+        token: function (stream, state) {
+            if (stream.match(/--\[\[/)) {
+                state.tokenize = longComment;
+                return state.tokenize(stream, state);
+            }
+            if (stream.match(/--/)) {
+                stream.skipToEnd();
+                return "comment";
+            }
+            if (stream.match(/"([^"\\]|\\.)*"?/)) {
+                return "string";
+            }
+            if (stream.match(/'([^'\\]|\\.)*'?/)) {
+                return "string";
+            }
+            if (stream.match(/\b\d+(\.\d+)?\b/)) {
+                return "number";
+            }
+            if (
+                stream.match(
+                    /\b(function|end|if|then|else|elseif|while|do|for|in|repeat|until|return|local|not|and|or)\b/
+                )
+            ) {
+                return "keyword";
+            }
+            if (stream.match(/[a-zA-Z_]\w*/)) {
+                return "variable";
+            }
+            stream.next();
+            return null;
+        },
+    };
+});
+
+CodeMirror.defineMIME("text/x-lua", "luau");
+CodeMirror.defineMIME("text/x-luau", "luau");
